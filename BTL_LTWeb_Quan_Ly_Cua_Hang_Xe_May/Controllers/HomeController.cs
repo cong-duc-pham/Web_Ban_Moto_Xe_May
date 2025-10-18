bcf5322e1dee4d81c6bd5e5c8ba65cf94b01498f
Vo-Thien-Linh <vothienlinh2@gmail.com>
2025-10-18 22:00:56 +0700

    Them project MVC vao repo

---
+++ BTL_LTWeb_Quan_Ly_Cua_Hang_Xe_May/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using BTL_LTWeb_Quan_Ly_Cua_Hang_Xe_May.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_LTWeb_Quan_Ly_Cua_Hang_Xe_May.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BTL_LTWeb_Quan_Ly_Cua_Hang_Xe_May/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTL_LTWeb_Quan_Ly_Cua_Hang_Xe_May.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
